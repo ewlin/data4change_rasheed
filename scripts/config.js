a61185ef081c9246e4cb9e83b382e3512364b5c7
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-08-01 12:30:36 +0300

    Final json format

--- scripts/config.js
+++ scripts/config.js
@@ -7,8 +7,11 @@ module.exports.SPREADSHEET_ID = '1V-INXvFo918hYdcVTZGTVkLdo9QDCHsc3A9Gx8mgH00';
   Structure of the tabs of the google sheet
  */
 module.exports.SHEETS = [
-  { index: 0, name: 'questions', cols: ['title', 'text', 'graphics'] },
-  { index: 1, name: 'data-vizes', cols: ['title', 'text', 'graphics'] },
+  { index: 0, type: 'object', name: 'quizz', cols: ['title'] },
+  { index: 1, type: 'array', name: 'questions', cols: ['text', 'icon', 'color', 'type', 'placeholder'] },
+  { index: 2, type: 'array', name: 'data-vizes', cols: ['title', 'text', 'image'] },
+  { index: 3, type: 'object', name: 'about', cols: ['title', 'text', 'subtitle',  'CTA', 'image'] },
+  { index: 4, type: 'array', name: 'form', cols: ['question', 'type', 'is required', 'data'] },
 ];
 
 /*
